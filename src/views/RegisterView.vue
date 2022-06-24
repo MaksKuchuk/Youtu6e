d1b496c79ff069b15dfced31d54b98134113bc90
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-06-25 02:20:51 +1000

    reg/auth

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,27 +2,26 @@
   <div class="banner w-full min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" >
     <div class="w-full sm:max-w-md p-5 mx-auto">
       <h2 class="mb-12 text-center text-5xl font-extrabold text-gray-200">Регистрация</h2>
-      <form>
+      <form @submit.prevent="register">
         <div class="mb-4">
           <label class="block mb-1 text-gray-200" for="nickname">Никнейм</label>
-          <input id="nickname" type="text" name="nickname" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+          <input v-model="formData.username" id="nickname" type="text" name="nickname" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
         </div>
         <div class="mb-4">
           <label class="block mb-1 text-gray-200" for="email">Почта</label>
-          <input id="email" type="text" name="" class="email bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+          <input v-model="formData.email" id="email" type="text" name="email" class="email bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
         </div>
         <div class="mb-4">
           <label class="block mb-1 text-gray-200" for="password">Пароль</label>
-          <input id="password1" type="password" name="password1" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+          <input v-model="formData.password" id="password" type="password" name="password" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
         </div>
         <div class="mb-4">
           <label class="block mb-1 text-gray-200" for="password">Повторите пароль</label>
-          <input id="password2" type="password" name="password2" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+          <input v-model="passwordCheck" id="passwordCheck" type="password" name="passwordCheck" class="bg-yellow-50 py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
         </div>
-        <div class="mt-6">
-          <router-link to="/login">
-            <button class="w-full inline-flex items-center justify-center px-4 py-2 bg-yellow-700 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-yellow-800 active:bg-red-700 focus:outline-none focus:border-red-700  disabled:opacity-25 transition">Зарегистрироваться</button>
-          </router-link>
+        <div class="mt-6">          
+          <button class="w-full inline-flex items-center justify-center px-4 py-2 bg-yellow-700 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-yellow-800 active:bg-red-700 focus:outline-none focus:border-red-700  disabled:opacity-25 transition"
+                  type="submit">Зарегистрироваться</button>          
         </div>
         <div class="mt-6 text-center text-white">
           <router-link to="/login" class="underline">Уже есть аккаунт? Войдите.</router-link>
@@ -33,7 +32,28 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        email: '',
+        password: '',   
+      },
+      passwordCheck: '',
+    }
+  },
+
+  methods: {
+    register() {      
+      axios.post('http://localhost:8080/api/v1/auth/users/', this.formData)
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error.request))
+    },
+  },
+};
 </script>
 
 <style>
