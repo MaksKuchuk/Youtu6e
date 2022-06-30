363d28892fd3ffb81e61037cc831e7ac0a83bbc0
MaksKuchuk <94689394+MaksKuchuk@users.noreply.github.com>
2022-07-01 01:29:28 +1000

    Update StudioView.vue

--- src/views/StudioView.vue
+++ src/views/StudioView.vue
@@ -2,7 +2,7 @@
   <HeaderView/>
   <AsideView/>
   <main class="md:ml-24 xl:ml-64 pt-20 px-5 pb-5 grid max-w-screen-2xl m-auto">
-    <form class="max-w-screen-2xl pt-7" v-on:submit="submitForm">
+    <form class="max-w-screen-2xl pt-7">
       <div class="font-bold uppercase pt-3 pb-3 pl-2 bg-red-100 rounded w-1/2">Добавить видео</div>
       <div class="flex pb-5 pt-5">
         <div class="w-1/2">
@@ -11,7 +11,7 @@
               <label class="block uppercase text-gray-700 text-xs font-bold mb-2">
                 Название
               </label>
-              <input class="appearance-none block w-full text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="" type="text" v-model="name">
+              <input class="appearance-none block w-full text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="" type="text" v-model="videoName">
             </div>
           </div>
           <div class="flex justify-start">
@@ -56,7 +56,6 @@ export default {
 
   data() {
     return {
-      name: null,
       message: '',
       progress: 0,
       document: null,
@@ -67,11 +66,6 @@ export default {
   },
 
   methods: {
-    submitForm: function (event){
-      event.preventDefault(),
-      event.target.reset()
-    },
-
     selectFile() {
       this.document = this.$refs.file.files.item(0)
     },
@@ -90,10 +84,16 @@ export default {
       formData.append('video', this.document)
       formData.append('preview', this.preview)
       formData.append('description', this.description)
+
+      this.videoName = ""
+      this.$refs.file.value = null
+      this.$refs.preview.value = null;
+      this.description = ""
+
       await axios.post('http://localhost:8080/api/v1/addvideo/', formData, {headers})
-          .catch(error => {
-            console.log(error.response)
-          })
+        .catch(error => {
+          console.log(error.response)
+        })
     }
   },
 
